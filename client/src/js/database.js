e6a604c0fd30d117f5ad601883ad64ce71bb5df5
Timothy Johnson <timjohnson1897@gmail.com>
2023-05-23 19:43:35 -0500

    Added code to database.js

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,37 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readwrite');
+  const store = tx.objectStore('jate');
+  try {
+    await store.put(content);
+    console.log('Data added to the database');
+  } catch (error) {
+    console.error('Error adding data', error);
+  }
+  
+  await tx.done;
+  db.close();
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const db = await initdb();
+  const tx = db.transaction('jate', 'readonly');
+  const store = tx.objectStore('jate');
+  
+  try {
+    const data = await store.getAll();
+    console.log('Data retrieved from the database', data);
+    return data;
+  } catch (error) {
+    console.error('Error retrieving data', error);
+  }
+  
+  await tx.done;
+  db.close();
+};
 
 initdb();
